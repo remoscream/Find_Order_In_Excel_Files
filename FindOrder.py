418155246d3e0db3edb7b7bd383e95f708afa2e9
remoscream <56284999+remoscream@users.noreply.github.com>
2022-04-28 12:35:49 +0900

    Complete function Program can search all the xlsx files and the sheets in the file Also it can give some error messages when no file exist in the folder or no result can be found

--- FindOrder.py
+++ FindOrder.py
@@ -1,26 +1,46 @@
 # coding: utf-8
 
 import os
+import glob
 from openpyxl import load_workbook
 
-string_need = input("Enter the order you want to find: \n")
 
+# Get current folder path
 folderpath = os.path.split(os.path.realpath(__file__))[0]
 
-filepath = folderpath + '\注文_1.xlsx'
+# Initialize file counter
+file_counter = 0
 
-# Load xlsx file
-data = load_workbook(filepath, read_only=True, keep_links=False)
+# Get filelist
+filelist = glob.glob(folderpath + "/*.xlsx")
 
-# Get sheet names in xlsx file
-sheetnames = data.sheetnames
+# Searching
+if len(filelist) == 0:
+    print('Error ! No order files exist in the folder !')
 
-for m in range(0, len(sheetnames)):
-    sheet_now = data[sheetnames[m]]
+else:
+    # Get order name from input
+    string_need = input("Enter the order you want to find: \n")
 
-    for n in range(1, sheet_now.max_row+1):
-        celldata = sheet_now.cell(n, 1).value   # Read data of each cell in first column
+    # Load xlsx file
+    for f in glob.glob(folderpath + "/*.xlsx"):
+        data = load_workbook(f, read_only=True, keep_links=False)
 
-        if celldata is not None and string_need in celldata:
-            print('row', n, 'in sheet :', sheetnames[m], ', Order Name:', celldata)
+        # Get sheet names in xlsx file
+        sheetnames = data.sheetnames
+
+        for m in range(0, len(sheetnames)):
+            sheet_now = data[sheetnames[m]]
+
+            for n in range(1, sheet_now.max_row+1):
+                celldata = sheet_now.cell(n, 1).value   # Read data of each cell in first column
+
+                if celldata is not None and string_need in celldata:
+                    print('row', n, 'in file :', os.path.split(f)[1], '(sheet:', sheetnames[m], '), Order Name:', celldata)
+                    file_counter = file_counter + 1
+
+    if file_counter == 0:
+        print('Order name:', string_need, 'not found in the files !')
+
+
 
